7563999a2f24837d0e7550ccaf248f01b6aaaa26
Ayoub Tadlaoui <olympiadtechfounder@gmail.com>
2024-04-04 17:31:52 +0000

    Structs 4/4: nested structs to represent more complex data

--- Fundamentals/Strucs/structs.go
+++ Fundamentals/Strucs/structs.go
@@ -6,15 +6,44 @@ import (
 )
 
 // Structs
-// Adress Struct
-type Adress struct {
-	street, state, city, zipCode string
-}
 
 // - Define a struct representing a person with fields like name, age, and email. ✅
 type Person struct {
 	name, company, email, sex string
 	age                       int
+	adress                    Adress
+	langs                     MainLanguages
+}
+
+// Adress Struct
+type Adress struct {
+	city, region, country string
+	zipcode               int
+}
+
+// Natural Languages languages
+type MainLanguages struct {
+	naturalLanguages, programminglanguages []string
+}
+
+type AllLanguages struct {
+	naturalLanguages, programminglanguages MainLanguages
+}
+
+//
+
+// -Method to initiate the values of Adress Struct
+func (a *Adress) adressMethod(city, region, country string, zip int) {
+	a.city = city
+	a.region = region
+	a.country = country
+	a.zipcode = zip
+}
+
+// -Method to initiate values of all languages:
+func (mainLan *MainLanguages) languageStack(nattyLang, progLang []string) {
+	mainLan.naturalLanguages = nattyLang
+	mainLan.programminglanguages = progLang
 }
 
 // - Define methods on the person struct to update its fields.
@@ -27,15 +56,31 @@ func Structures() {
 	actualYear := time.Now().Year()
 	birthYear := 1995
 	sex := "Male"
+	//Languages:
+	nattylangs := [...]string{"English", "French", "Arabic"}
+	proglangs := []string{"Go", "Dart"}
+	mainLangs := MainLanguages{}
+	mainLangs.languageStack(nattylangs[:], proglangs)
 
+	//Adress
+	city, region, country := "Oujda", "Oriental Metropolitan Aria", "Morocco"
+	zip := 60000
+	adress := Adress{}
+	adress.adressMethod(city, region, country, zip)
 	//- Create instances of the person struct and print their details.✅
 	person := Person{
 		name:    "Atlas Kaisar",
 		company: "KaisarInov8ors",
 		email:   "atlas.kaisar@icloud.com",
 		age:     actualYear - birthYear,
+		adress:  adress,
+		langs:   mainLangs,
 	}
-	person.gender(sex)
-	fmt.Printf("______Boss Data______\nName:%s\nCompany:%s\nEmail:%s\nAge:%d\nSex:%s\n", person.name, person.company, person.email, person.age, person.sex)
 
+	person.gender(sex)
+	fmt.Printf("______Boss Data______\nName:%s\nCompany:%s\nEmail:%s\nAge:%d\nSex:%s\n%+v\n%+v\n", person.name, person.company, person.email, person.age, person.sex, person.adress, person.langs)
+
+	fmt.Println("===Adress===")
+	fmt.Println("", adress.city, "\n", adress.region, "\n", adress.country, "\n", adress.zipcode)
+	fmt.Printf("%+v\n", adress)
 }
