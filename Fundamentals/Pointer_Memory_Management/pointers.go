6ac130fb30ed8b31882bebe0e678cb4c82f404b2
Ayoub Tadlaoui <olympiadtechfounder@gmail.com>
2024-04-05 15:33:32 +0000

    Create a program that demonstrates the use of pointers to swap the values of two variables: ✅

--- Fundamentals/Pointer_Memory_Management/pointers.go
+++ Fundamentals/Pointer_Memory_Management/pointers.go
@@ -7,4 +7,24 @@ import (
 func Pointers() {
 	fmt.Println("-------------------- Pointers and Memory Managemnt -----------------------")
 	fmt.Println(" - Create a program that demonstrates the use of pointers to swap the values of two variables.")
+
+	x := "Before "
+	y := "Switch"
+	z := "After "
+	var val1, val2 int
+	val1 = 5
+	val2 = 2024
+	fmt.Println(x+y, "\nvalue 1: ", val1, "\nvalue 2: ", val2)
+	swap := func(a, b *string, v1, v2 *int) {
+		temp := *a
+		*a = *b
+		*b = temp
+
+		temp2 := *v1
+		*v1 = *v2
+		*v2 = temp2
+
+	}
+	swap(&x, &z, &val1, &val2)
+	fmt.Println(x+y, "\nvalue 1: ", val1, "\nvalue 2: ", val2)
 }
