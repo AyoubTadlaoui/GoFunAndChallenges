c1e7e50e14d72db86bb28a9e9c38a9c2649b2bdd
Ayoub Tadlaoui <olympiadtechfounder@gmail.com>
2024-04-05 15:51:51 +0000

    - Write a function that accepts a pointer to an integer and increments its value : ✅

--- Fundamentals/Pointer_Memory_Management/pointers.go
+++ Fundamentals/Pointer_Memory_Management/pointers.go
@@ -23,8 +23,16 @@ func Pointers() {
 		temp2 := *v1
 		*v1 = *v2
 		*v2 = temp2
-
 	}
+
 	swap(&x, &z, &val1, &val2)
 	fmt.Println(x+y, "\nvalue 1: ", val1, "\nvalue 2: ", val2)
+
+	fmt.Println("- Write a function that accepts a pointer to an integer and increments its value.")
+	x2 := 0
+	incrementPointer := func(a *int) {
+		*a++
+	}
+	incrementPointer(&x2)
+	fmt.Println(x2)
 }
